e87c96102ee6fbbd0bb4093c8ae76954d77cd448
ameek <ameek.aziz@gmail.com>
2023-11-29 03:24:50 +0600

    purchase history db connection up #26

--- src/microservices/purchase-history-service/modules/app.module.ts
+++ src/microservices/purchase-history-service/modules/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
+import { dataSourceOptions } from 'ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_PIPE } from '@nestjs/core';
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    }],
 })
 export class AppModule {}
